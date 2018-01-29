7818c2223f164be2178a671620a7abae55b769ba
JeffDChapman <jeffdchapman@hotmail.com>
2018-01-29 09:25:26 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Digital
+{
+    partial class DigitalClock
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private System.Windows.Forms.Timer tmrShowTime;
+        private System.Windows.Forms.Label lblTime = new System.Windows.Forms.Label();
+        
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TimeLable = new System.Windows.Forms.Label();
+            this.SecondTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // TimeLable
+            // 
+            this.TimeLable.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.TimeLable.AutoSize = true;
+            this.TimeLable.Font = new System.Drawing.Font("Britannic Bold", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimeLable.Location = new System.Drawing.Point(78, 52);
+            this.TimeLable.Margin = new System.Windows.Forms.Padding(200);
+            this.TimeLable.Name = "TimeLable";
+            this.TimeLable.Size = new System.Drawing.Size(125, 30);
+            this.TimeLable.TabIndex = 0;
+            this.TimeLable.Text = "00:00:00";
+            this.TimeLable.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.TimeLable.Leave += new System.EventHandler(this.TimeLable_Leave);
+            this.TimeLable.MouseEnter += new System.EventHandler(this.TimeLable_MouseEnter);
+            // 
+            // SecondTimer
+            // 
+            this.SecondTimer.Enabled = true;
+            this.SecondTimer.Interval = 250;
+            this.SecondTimer.Tick += new System.EventHandler(this.SecondTimer_Tick);
+            // 
+            // DigitalClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 134);
+            this.ControlBox = false;
+            this.Controls.Add(this.TimeLable);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Location = new System.Drawing.Point(1500, 500);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DigitalClock";
+            this.Opacity = 0.5D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.ResizeEnd += new System.EventHandler(this.DigitalClock_ResizeEnd);
+            this.MouseEnter += new System.EventHandler(this.DigitalClock_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.DigitalClock_MouseLeave);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TimeLable;
+        private System.Windows.Forms.Timer SecondTimer;
+
+     }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace Digital
+{
+    public partial class DigitalClock : Form
+    {
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        bool TurnDownVol = false;
+        bool TurnUpVol = false;
+
+        public DigitalClock()
+        {
+            InitializeComponent();
+        }
+
+        private void SecondTimer_Tick(object sender, EventArgs e)
+        {
+            this.TimeLable.Text = DateTime.Now.ToString("hh:mm:ss");
+            if (TurnDownVol)
+                {keybd_event((byte)Keys.VolumeDown, 0, 0, 0);}
+            if (TurnUpVol)
+                { keybd_event((byte)Keys.VolumeUp, 0, 0, 0); }
+        }
+
+        private void DigitalClock_ResizeEnd(object sender, EventArgs e)
+        {
+            int formH = this.Height;
+            int formW = this.Width;
+            this.TimeLable.Top = (this.Height / 2) - 25;
+            this.TimeLable.Left = (this.Width / 2) - 70;
+        }
+
+        private void SoundCode()
+        {
+            keybd_event((byte)Keys.VolumeUp, 0, 0, 0); // increase volume
+            keybd_event((byte)Keys.VolumeDown, 0, 0, 0); // decrease volume}
+        }
+
+        private void DigitalClock_MouseEnter(object sender, EventArgs e)
+        {
+            TurnUpVol = false;
+            TurnDownVol = true;
+        }
+
+        private void DigitalClock_MouseLeave(object sender, EventArgs e)
+        {
+            TurnDownVol = false;
+        }
+
+        private void TimeLable_MouseEnter(object sender, EventArgs e)
+        {
+            TurnDownVol = false;
+            TurnUpVol = true;
+        }
+
+        private void TimeLable_Leave(object sender, EventArgs e)
+        {
+            TurnUpVol = false;
+        }
+    }
+}
